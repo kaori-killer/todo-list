d12b6c6999aa2f2fa0f88ddc30481820ce01b4d5
kaori-killer <sksbthwjd@daum.net>
2023-02-24 16:22:39 +0900

    feat: 추가되는 할 일을 ul태그 안에 삽입한다.

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,8 @@ const getStringDate = (date) => {
 
 // - [x] 새로운 할 일을 확인 버튼으로 추가한다.
 // - [x] 새로운 할 일을 엔터키 입력으로 추가한다.
-// - [] 추가되는 할 일을 ul태그 안에 삽입한다.
-// - [] 총 할일 개수를 세어 상단에 보여주낟.
+// - [x] 추가되는 할 일을 ul태그 안에 삽입한다.
+// - [] 총 할일 개수를 세어 상단에 보여준다.
 // - [] 할 일이 추가되고 나면 input은 빈 값으로 초기화한다.
 // - [] 사용자 입력 값이 빈 값이라면 추가되지 않는다.
 
@@ -19,6 +19,7 @@ function App() {
 
     this.init = () => {
         changeDate(this.curDate);
+        render();
     }
 
     const changeDate = (newDate) => {
@@ -43,6 +44,7 @@ function App() {
         }
         const todoName = $("#todo-name").value;
         this.todoList.push(todoName);
+        render();
     }
 
     $("#day-decrease-button").addEventListener("click", (e)=>{
@@ -63,6 +65,37 @@ function App() {
     });
 
     $("#todo-submit-button").addEventListener("click", addTodo);
+
+    const render = () => {
+        const template = this.todoList
+        .map((item, index)=>{
+                return (
+                    `<li class="data-todo-id=${index} todo-list-item d-flex items-center py-2">
+                    <span class="w-100 pl-2 todo-name sold-out">${item}</span>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+                    >
+                    완료
+                    </button>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 todo-edit-button"
+                    >
+                    수정
+                    </button>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm todo-remove-button"
+                    >
+                    삭제
+                    </button>
+                </li>`
+            );
+        }).join(""); 
+        console.log(this.todoList)
+        $("#todo-list").innerHTML = template;
+    }
 }
 
 const app = new App();
